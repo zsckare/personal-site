303fa168989140adc0e9aaecf9698d07527d7d96
Antonio Alvarez <tokyo.blue.tsubasa@gmail.com>
2018-03-13 22:03:25 -0600

    add normalize, typewritterjs, and few changes in styles, add images to background

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var app = document.getElementById('write');
+
+var typewriter = new Typewriter(app, {
+    loop: false
+});
+
+typewriter.typeString("I'm a Web Developer.")
+    .pauseFor(2500)
+    .deleteChars(14)
+    .typeString('Mobile App Developer.')
+    .pauseFor(2500)
+    .deleteChars(21)
+    .typeString('Geek.')
+    .pauseFor(3000)
+    .deleteAll()
+    .typeString("I'm Antonio Alvarez.")
+    .start();
